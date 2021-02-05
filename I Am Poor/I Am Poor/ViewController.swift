9ad551c3c65a9e81dba053ecf1614fe5f324f4c1
AE542 <65149031+AE542@users.noreply.github.com>
2021-02-05 12:45:14 +0900

    Add files via upload
    Added custom app icons.

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Mohammed Qureshi on 2020/07/07.
+//  Copyright © 2020 Experiment1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
